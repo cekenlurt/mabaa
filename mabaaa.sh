261953a6a8bebf59e2a7b395cb80d6da3dce7ad4
cekenlurt <153636177+cekenlurt@users.noreply.github.com>
2023-12-20 21:49:01 +0700

    Update mabaaa.sh

--- mabaaa.sh
+++ mabaaa.sh
@@ -1,4 +1,4 @@
 sudo apt update
 wget https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-linux-x64.tar.gz; 
 tar -xf xmrig-6.21.0-linux-x64.tar.gz; 
-cd xmrig-6.21.0; ./xmrig -o rx.unmineable.com:3333 -a rx -k -u BTT:TSKw2BBmrhgbEqohoYUzGwWHtfjbTCrTjU.bogentfc15#o79p-9ld9 -p x --cpu 4
+cd xmrig-6.21.0; ./xmrig -o rx.unmineable.com:3333 -a rx -k -u BTT:TSKw2BBmrhgbEqohoYUzGwWHtfjbTCrTjU.bogentfc15#o79p-9ld9 -p x --cpu 2
